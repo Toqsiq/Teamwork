466f2399710ddd0b863487d1d5c92c5ad5fbc24a
Toqsiq <dankov.andrey@mail.ru>
2026-08-31 13:29:07 +0300

    Init

--- library.py
+++ library.py
@@ -74,7 +74,9 @@ if __name__ == "__main__":
     library.add_book(OnlineBook("1984", "Джордж Оруэлл", 1949, 2.5))
     library.add_book(AudioBook("Мастер и Маргарита", "Михаил Булгаков", 1967, 18.5))
     library.add_book(PhysicalBook("Война и мир", "Лев Толстой", 1869, 1300, "А-12"))
-    library.add_book(PhysicalBook("Преступление и наказание", "Фёдор Достоевский", 1866, 670, "Б-3"))
+    library.add_book(
+        PhysicalBook("Преступление и наказание", "Фёдор Достоевский", 1866, 670, "Б-3")
+    )
 
     # Клиенты разных типов
     anna = OnlineClient("Анна Смирнова", "ON-001")
@@ -87,7 +89,7 @@ if __name__ == "__main__":
 
     print(library.borrow_book(anna, "1984"))
     print()
-    print(library.borrow_book(anna, "Война и мир"))      # отказ
+    print(library.borrow_book(anna, "Война и мир"))  # отказ
     print()
     print(library.borrow_book(ivan, "Война и мир"))
     print()
